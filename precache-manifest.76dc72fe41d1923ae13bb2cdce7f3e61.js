a5e1ed223eb6e56fa862688d90a413b922e10f66
Oliver <olsantia@asu.edu>
2018-12-02 14:57:34 -0800

    Updates

--- precache-manifest.76dc72fe41d1923ae13bb2cdce7f3e61.js
+++ precache-manifest.76dc72fe41d1923ae13bb2cdce7f3e61.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/my_website/static/js/runtime~main.4b706c50.js"
   },
   {
-    "revision": "6bac4244411161379847",
-    "url": "/my_website/static/js/main.6bac4244.chunk.js"
+    "revision": "dfd0d3000561d93de6f5",
+    "url": "/my_website/static/js/main.dfd0d300.chunk.js"
   },
   {
     "revision": "56566b047b9432b94474",
     "url": "/my_website/static/js/1.56566b04.chunk.js"
   },
   {
-    "revision": "6bac4244411161379847",
+    "revision": "dfd0d3000561d93de6f5",
     "url": "/my_website/static/css/main.9dd89c91.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/my_website/static/css/1.2a2545b4.chunk.css"
   },
   {
-    "revision": "28bf3d7d38d00a5b77286966eaf715af",
+    "revision": "ef095c5a856fcacb3a0ec43ca7e4be89",
     "url": "/my_website/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my_website/precache-manifest.98efc77157c6cf7d48c95be406fd6954.js"
+  "/my_website/precache-manifest.76dc72fe41d1923ae13bb2cdce7f3e61.js"
 );
 
 workbox.clientsClaim();
