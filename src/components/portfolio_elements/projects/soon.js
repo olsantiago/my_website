323570406389a37bcb7448783c82871a0cf2320b
Oliver <olsantia@asu.edu>
2019-05-19 23:43:58 -0700

    Initial Commit

--- src/components/portfolio_elements/projects/soon.js
+++ src/components/portfolio_elements/projects/soon.js
@@ -7,12 +7,12 @@ class Soon extends Component{
         return(
                 <div>
                 <Card className='card' shadow={0} style={{width: '220px', height: '300px', margin:'auto', borderRadius:'0.3em', boxShadow: '0 0 8px 0px #ab9fd4'}}>
-                    <CardTitle className='card_title' expand style={{ color: 'black', backgroundColor:'black', background: 'url(https://cdn.worldvectorlogo.com/logos/angular-icon.svg) center / cover'}}>Coming Soon</CardTitle>
+                    <CardTitle className='card_title' expand style={{ color: 'black', backgroundColor:'black', background: 'url(https://cdn.worldvectorlogo.com/logos/angular-icon.svg) center / cover'}}>Dopweb Builder</CardTitle>
                         <CardText style={{width:'100%', height:'70px', color:'white', backgroundColor:'#ab9fd4'}}>
-                            Project in progress....
+                            Simplest yet poweful Website Builder
                         </CardText>
                     <CardActions border style={{backgroundColor:'#ab9fd4'}}>
-                         <Button colored ripple style={{color:'white', backgroundColor:'black', borderRadius:'25px'}}>Check Out</Button>
+                    <a href='https://builder.dopweb.com/' target='_blank' rel='noopener noreferrer'><Button colored ripple style={{color:'white', backgroundColor:'black', borderRadius:'25px'}}>Check Out</Button></a>
                     </CardActions>
                 </Card>
                 </div>
